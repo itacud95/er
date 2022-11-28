b2f6410bc8e6dfb295a1d8b32eeda5ea5c4986d0
Jakob <jakobhkrohn@gmail.com>
2022-11-28 19:59:34 +0100

    Add simple example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use shell_completion::{BashCompletionInput, CompletionInput, CompletionSet};
+
 fn main() {
-    println!("Hello, world!");
+    let input = BashCompletionInput::from_env()
+        .expect("Missing expected environment variables");
+
+    let completions = input.complete_subcommand(vec!["add", "commit"]);
+
+    completions.suggest();
 }
