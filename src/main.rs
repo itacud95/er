7d47dfba024317abb7cebcd240b949cb789b524f
Jakob <jakobhkrohn@gmail.com>
2022-11-28 23:22:25 +0100

    Check input length

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,15 @@ fn complete() {
 }
 
 fn write_out(input: BashCompletionInput) {
-    let completions = input.complete_subcommand(vec!["add", "commit", "build", "test"]);
-    completions.suggest();
+    let first_completions = vec!["add", "commit", "build", "test"];
+    match input.arg_index() {
+        1 => complete_string(input, first_completions),
+        _ => complete_string(input, vec!["second"]),
+    }
     exit(0)
+}
+
+fn complete_string(input: BashCompletionInput, txt: Vec<&str>) {
+    let completions = input.complete_subcommand(txt);
+    completions.suggest();
 }
