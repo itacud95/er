74a98b24f4d967b56c29b9e63342978714042c55
Jakob <jakobhkrohn@gmail.com>
2022-12-19 14:26:36 +0100

    Basic tab comletion in layers

--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,43 @@ impl<'a> Command<'a> {
     pub fn get_sub_commands(&self) -> Vec<String> {
         let mut commands = vec![];
         for name in self.childs.iter() {
+            //println!("{}::sub->{}", self.command, name.value());
             commands.push(name.value());
         }
         return commands;
     }
-    pub fn get_commands(&self) -> Vec<&str> {
-        let mut commands = vec![self.command];
-        // let mut commands = vec![self.command];
-        // let mut commands = String::from(self.command);
-
-        for name in self.childs.iter() {
-            commands.append(&mut name.get_commands())
-            // commands.push_str(", ");
-            // commands.push_str(name.get_commands().as_str());
+    pub fn contains(&self, cmd: &str) -> bool {
+        for command in self.get_sub_commands() {
+            if cmd == command {
+                return true;
+            }
         }
-        return commands;
+        return false;
     }
-    pub fn add_sub_command(&mut self, cmd: &'a Command<'a>) {
-        self.childs.push(cmd);
+    pub fn get_string(&self, current_args: &Vec<&str>) -> Vec<String> {
+        //println!("X{}", current_args.len());
+
+        let mut num = 0;
+        // for arg in current_args.iter().rev() {
+        for arg in current_args {
+            // println!("#{} {}#", num, arg);
+            num += 1;
+
+            for cmd in self.childs.iter() {
+                if arg == &cmd.value() {
+                    // println!("{}=={}", arg, &cmd.value());
+                    // if (cmd.contains(arg))
+                    // if (cmd.childs.len() > 1) {
+                    return cmd.get_string(current_args);
+                    // }
+                    // return cmd.get_sub_commands();
+                }
+            }
+        }
+
+        // println!("EmptyReturn");
+        return self.get_sub_commands();
+        // return vec![];
     }
     pub fn value(&self) -> String {
         self.command.to_string()
@@ -55,59 +74,20 @@ fn complete() {
     };
 }
 
-fn create_cmd_without_subcommands<'a>(cmd: &str) -> Command<'a> {
-    return Command::new(Box::new("sdb_basic_test"), Box::new(vec![]));
-}
-
-fn generate_from_index(index: u64, commands: Vec<Command>) -> Vec<String> {
-    let mut output = Vec::new();
-    for cmd in commands.iter() {
-        output.push(cmd.value())
-    }
-
-    return output;
-}
-
-fn generate_from_args(args: Vec<&str>, commands: Vec<Command>) -> Vec<String> {
-    let currentArg = args.first().expect("It is not here?");
-
-    // println!("X{}", args.len());
-    // for arg in &args {
-    //     println!("#{}#", arg);
-    // }
-
-    let mut output = Vec::new();
-
-    if args.len() < 4 {}
-
-    for cmd in commands.iter() {
-        if currentArg == &cmd.value() {
-            return cmd.get_sub_commands();
-        }
-        // return cmd.get_sub_commands();
-        // output.push(cmd.value())
-        // let search = cmd.childs;
-        // search.contains();
-    }
-
-    for cmd in commands.iter() {
-        output.push(cmd.value())
-        // return cmd.get_sub_commands()
-    }
-
-    return output;
-}
+// fn create_cmd_without_subcommands<'a>(cmd: &str) -> Command<'a> {
+//     return Command::new(Box::new(cmd), Box::new(vec![]));
+// }
 
 fn write_out(input: &BashCompletionInput) {
-    let sdk_basic_test_cmd = create_cmd_without_subcommands("sdk_basic_test");
-    let elf_test_cmd = create_cmd_without_subcommands("elf_test");
-    let obfuscated_ptr_test_cmd = create_cmd_without_subcommands("obfuscated_ptr_test");
+    let sdk_basic_test_cmd = Command::new(Box::new("sdk_basic_test"), Box::new(vec![]));
+    let elf_test_cmd = Command::new(Box::new("elf_test"), Box::new(vec![]));
+    let obfuscated_ptr_test_cmd = Command::new(Box::new("obfuscated_test"), Box::new(vec![]));
 
     let shield_cmd = Command::new(
         Box::new("shield"),
         Box::new(vec![
-            &sdk_basic_test_cmd,
             &elf_test_cmd,
+            &sdk_basic_test_cmd,
             &obfuscated_ptr_test_cmd,
         ]),
     );
@@ -115,25 +95,25 @@ fn write_out(input: &BashCompletionInput) {
     let build_cmd = Command::new(Box::new("build"), Box::new(vec![&shield_cmd]));
     let log_cmd = Command::new(Box::new("log"), Box::new(vec![]));
 
-    let commands = vec![build_cmd, log_cmd];
+    let current_args = input.args().clone();
 
-    let args = input.args().clone();
+    let root_cmd = Command::new(Box::new(""), Box::new(vec![&build_cmd, &log_cmd]));
+    let output = root_cmd.get_string(&current_args);
+    complete_string(input, output);
+
     // generate_from_args(args, commands);
     // let c = args.clone();
 
-    match input.arg_index() {
-        _ => complete_string(input, generate_from_args(args, commands)),
-        // _ => complete_string(input, generate_from_index(0, commands)),
-        // _ => complete_string(input, build_cmd.get_sub_commands()),
-        // 1 => complete_string(input, Vec::from_iter(commands[0..1].iter().cloned())),
-        // _ => complete_string(input, Vec::from_iter(commands[1..2].iter().cloned())),
-    }
+    // match input.arg_index() {
+    //     _ => complete_string(input, generate_from_args(args, commands)),
+    // }
     exit(0)
 }
 
 fn complete_string(input: &BashCompletionInput, txt: Vec<String>) {
     let mut commands = vec![""];
     for cmd in txt.iter() {
+        //println!("comp::{}", cmd);
         commands.push(cmd.as_str())
     }
 
